04a512fbd19492e6f094f060e4c58739e245bf9f
Niket Patel <niketpatel2525@gmail.com>
2018-06-01 09:39:30 -0500

    fix(module-api): corsorigin error fix

--- src/main/java/io/ennate/trucker/CORSFilter.java
+++ src/main/java/io/ennate/trucker/CORSFilter.java
@@ -13,9 +13,11 @@ public class CORSFilter extends GenericFilterBean implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE, PATCH");
-        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Max-Age", "6000");
         response.setHeader("Access-Control-Expose-Headers", "Location");
+        response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
         chain.doFilter(req, res);
     }
 }
